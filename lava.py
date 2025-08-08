44106a999e6f737cdeb115a9101cedea44b9f875
eddymadfdfd <13354906055@163.com>
2025-08-09 07:41:06 +0800

    Add files via upload

---
+++ lava.py
@@ -0,0 +1,42 @@
+import requests
+import time
+import json
+
+# 多个钱包地址
+wallet_addresses = ['0x.....', '0x......', '0x......', '0x......']
+
+# ETH主网的RPC接口URL
+rpc_url = 'https://eth1.lava.build/XXXXX/'
+
+while True:
+    for wallet_address in wallet_addresses:
+        # 创建JSON-RPC请求
+        request = {
+            'jsonrpc': '2.0',
+            'method': 'eth_getBalance',
+            'params': [wallet_address, 'latest'],
+            'id': 1,
+        }
+
+        # 发送JSON-RPC请求
+        response = requests.post(rpc_url, json=request)
+
+        # 检查响应
+        if response.status_code == 200:
+            # 解析响应的JSON数据
+            response_data = response.json()
+
+            # 检查"result"键是否存在
+            if 'result' in response_data:
+                # 将余额从十六进制转换为十进制
+                balance = int(response_data['result'], 16)
+                # 打印余额
+                print(f'ETH balance for {wallet_address}: {balance}')
+            else:
+                # 打印整个响应数据
+                print(f'Error querying ETH balance for {wallet_address}: "result" key not found in response data. Response data: {response_data}')
+        else:
+            print(f'Error querying ETH balance for {wallet_address}: {response.status_code}')
+
+    # 等待3分钟
+    time.sleep(10)
